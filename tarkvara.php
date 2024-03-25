ff67e18cc0704063692c6bca7909c02ad1baf263
opilane <timur.denisenko.2887@gmail.com>
2024-03-25 16:22:18 +0200

    loo tabel ja loo uue fail

--- tarkvara.php
+++ tarkvara.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="tarkvara.css">
     <title>Tarkvara sõnad</title>
     <script>
         const synad = {"teostus":"исполнение","algoritm":"алгоритм","kavand":"план","kasutajaliides":"пользовательский интерфейс",
@@ -13,6 +14,11 @@
             "koskmudel":"каскадная модель","agiilne mudel":"агильная модель","spiraalne mudel":"спиральная модель","inkrementaalne mudel":"инкрементальная модель","nõudmised":"требования",
             "realiseerimine":"реализация","testimime":"тестирование","integreerimine":"интегригование","kasutamine":"использование","hooldus":"уход","eelised":"преимущества",
             "puudused":"недостатки"}
+        function clear()
+        {
+            document.getElementById("kontroll").style.color = "black"
+            document.getElementById("kontroll").style.backgroundColor = "white"
+        }
         function randomSuna()
         {
 
@@ -20,29 +26,46 @@
             const juhuslikSyna=keys[Math.floor(Math.random()*keys.length)]
             document.getElementById("random-syna").innerHTML = juhuslikSyna
         }
-        function kontroll()
+        async function kontroll()
         {
             const syna = document.getElementById("kontroll").value.toLowerCase()
-            let vastus
             if (syna === synad[document.getElementById("random-syna").innerHTML])
             {
-                vastus = "õige"
-                document.getElementById("vastus").style.color = "green"
+                document.getElementById("kontroll").style.color = "white"
+                document.getElementById("kontroll").style.backgroundColor = "green"
+                let table = document.getElementById("table");
+                let row = table.insertRow(1);
+                let c2 = row.insertCell(0);
+                let c1 = row.insertCell(1);
+                c1.innerText = syna
+                c2.innerText = document.getElementById("random-syna").innerHTML
+                randomSuna()
+                await new Promise(resolve => setTimeout(resolve, 1000))
+                clear()
             }
             else
             {
-                vastus = "vale"
-                document.getElementById("vastus").style.color = "red"
+                document.getElementById("kontroll").style.color = "white"
+                document.getElementById("kontroll").style.backgroundColor = "red"
             }
-            document.getElementById("vastus").innerHTML = vastus
         }
     </script>
 </head>
 <body onload="randomSuna()">
-<input type="button" value="Uuenda sõna" onclick="randomSuna()">
-<div id="random-syna"></div><br>
-<label for="kontroll">Kirjuta sõna vene keeles</label>
-<input type="text" name="kontroll" id="kontroll" onchange="kontroll()">
-<div id="vastus"></div>
+<table id="table">
+    <tr>
+        <td>Eesti keel</td>
+        <td>Vene keel</td>
+    </tr>
+<tr>
+    <td>
+        <div id="random-syna"></div>
+        <input type="button" value="Uuenda sõna" onclick="randomSuna()">
+    </td>
+    <td>
+        <input type="text" name="kontroll" id="kontroll" onchange="kontroll()" onselect="clear()">
+    </td>
+</tr>
+</table>
 </body>
 </html>
